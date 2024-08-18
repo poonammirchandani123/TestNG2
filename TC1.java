b22b26a9cad18b1e2f845ab02d57fc4699b48528
poonammirchandani123 <147245184+poonammirchandani123@users.noreply.github.com>
2024-08-18 20:51:37 +0100

    Add files via upload

---
+++ TC1.java
@@ -0,0 +1,34 @@
+import org.testng.annotations.*;
+
+public class TC1 {
+
+    @BeforeClass
+    void beforeClass() {
+        System.out.println("This will execute before the class...");
+    }
+
+    @AfterClass
+    void afterClass() {
+        System.out.println("This will execute after the class...");
+    }
+    @BeforeMethod
+    void beforeMethod() {
+        System.out.println("This will execute before every method...");
+    }
+
+    @AfterMethod
+    void afterMethod()  {
+        System.out.println("This will execute after every method...");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("This is test1...");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("This is test2...");
+    }
+
+}
